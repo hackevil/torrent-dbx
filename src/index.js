76735817a8172732ab2ed1b256555e6c1518ef4d
Kevin Ndung'u <kevgathuku@gmail.com>
2017-03-19 00:47:12 +0300

    Replace skeleton CSS with bootstrap

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {useStrict} from 'mobx';
-import 'skeleton-css/css/normalize.css';
-import 'skeleton-css/css/skeleton.css';
+
+import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.css';
+
+window.jQuery = $;
+require('bootstrap');
 
 import App from './App';
 import appStore from './store';
